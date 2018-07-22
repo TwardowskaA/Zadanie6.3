c1d763cb442fee509301ae171423b7245e02f2a9
Agata <twardowska123@gmail.com>
2018-07-22 20:14:00 +0200

    zadanie

--- src/ArrayComparator.java
+++ src/ArrayComparator.java
@@ -2,16 +2,16 @@ public class ArrayComparator {
 
     boolean compare(int[] tab1, int[] tab2) {
 
+        if (tab1.length != tab2.length) {
+            return false;
+        }
+
         for (int i = 0; i < tab1.length; i++) {
 
             if (tab1[i] != tab2[i]) {
                 return false;
             }
 
-            if (tab1.length != tab2.length) {
-                return false;
-
-            }
         }
         return true;
     }
